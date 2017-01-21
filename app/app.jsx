680d4cebf271d1c99e2ae39ed2dd77aa498e4aac
Wilmer Guerra <wilmer@targetcw.com>
2017-01-20 23:27:28 -0800

    created main container component

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var { Route, Router, IndexRoute, hashHistory } = require('react-router');
+
+var TodoApp = require('TodoApp');
 // Load foundation
 $(document).foundation();
 
@@ -8,6 +10,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-    <p>React Boilerplate</p>,
+    <TodoApp />,
     document.getElementById('app')
 );
