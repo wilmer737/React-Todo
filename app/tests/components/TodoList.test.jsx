ce70bc62fb5226e2bcd1a4624811660cf19ba6bc
Wilmer Guerra <wilmer@targetcw.com>
2017-01-21 00:05:47 -0800

    Finalized test suite for list componeent

--- app/tests/components/TodoList.test.jsx
+++ app/tests/components/TodoList.test.jsx
@@ -5,9 +5,18 @@ var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
 
 var TodoList = require('TodoList');
+var Todo = require('Todo');
 
 describe('TodoList', () => {
     it('should exist', () => {
         expect(TodoList).toExist();
-    })
+    });
+
+    it('should render 1 todo component for each todo item', () => {
+       var todos = [{id: 1, text: 'do something'}, {id: 2, text: 'check mail'}];
+       var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+       var todoComponents = TestUtils.scryRenderedComponentsWithType(todoList, Todo);
+
+       expect(todoComponents.length).toBe(todos.length);
+    });
 });
