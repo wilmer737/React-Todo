6e943b9e98989295f1926104bf56df36f756444d
Wilmer Guerra <wilmer@targetcw.com>
2017-01-20 23:00:56 -0800

    initial commit

---
+++ app/app.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var { Route, Router, IndexRoute, hashHistory } = require('react-router');
+// Load foundation
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles');
+
+ReactDOM.render(
+    <p>React Boilerplate</p>,
+    document.getElementById('app')
+);
